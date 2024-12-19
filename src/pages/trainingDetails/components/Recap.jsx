096bb016f4823623f1e722def760ea1afc8a80de
Nabil Yahyaoui <yahyaouinabil1993@gmail.com>
2024-12-19 17:01:09 +0100

    fix hero training details page

--- src/pages/trainingDetails/components/Recap.jsx
+++ src/pages/trainingDetails/components/Recap.jsx
@@ -2,22 +2,26 @@ import React from "react";
 
 const Recap = () => {
   return (
-    <div className="grid grid-cols-4 gap-4 divide-x w-fit h-24 px-6 lg:px-8 z-10 md:-mt-12 mx-auto rounded-md bg-white shadow-2xl">
-      <div className="flex flex-col gap-2 px-4 justify-center items-center text-center text-gray-600">
-        <p className="text-lg font-bold">Contenus</p>
-        <p>4 sections</p>
+    <div className="grid lg:grid-cols-4 grid-cols-2 gap-4 lg:divide-x w-fit py-4 px-2 md:px-8 z-10 -mt-12 mx-auto rounded-md bg-white shadow-2xl">
+      <div className="flex flex-col gap-2 sm:px-4 px-2 justify-center items-center text-center text-gray-600">
+        <p className="lg:text-lg sm:text-base text-sm font-bold">Contenus</p>
+        <p className="sm:text-base text-sm">4 sections</p>
       </div>
-      <div className="flex flex-col gap-2 px-4 justify-center text-center text-gray-600">
-        <p className="text-lg font-bold">Référence action DPC</p>
-        <p>93292325124</p>
+      <div className="flex flex-col gap-2 sm:px-4 px-2 justify-center text-center text-gray-600">
+        <p className="lg:text-lg sm:text-base text-sm font-bold">
+          Référence action DPC
+        </p>
+        <p className="sm:text-base text-sm">93292325124</p>
       </div>
-      <div className="flex flex-col gap-2 px-4 justify-center text-center text-gray-600">
-        <p className="text-lg font-bold">Durée</p>
-        <p>15 heures</p>
+      <div className="flex flex-col gap-2 sm:px-4 px-2 justify-center text-center text-gray-600">
+        <p className="lg:text-lg sm:text-base text-sm font-bold">Durée</p>
+        <p className="sm:text-base text-sm">15 heures</p>
       </div>
-      <div className="flex flex-col gap-2 px-4 justify-center text-center text-gray-600">
-        <p className="text-lg font-bold">Financements</p>
-        <p>DPC, FIF PL</p>
+      <div className="flex flex-col gap-2 sm:px-4 px-2 justify-center text-center text-gray-600">
+        <p className="lg:text-lg sm:text-base text-sm font-bold">
+          Financements
+        </p>
+        <p className="sm:text-base text-sm">DPC, FIF PL</p>
       </div>
     </div>
   );
