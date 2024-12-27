1ffe2e0aa23655513451df675577345adce119e8
amine <dridiimedamine@gmail.com>
2024-12-27 11:41:12 +0100

    last-update

--- best-distritbution/client/run.sh
+++ best-distritbution/client/run.sh
@@ -1,1 +1,1 @@
-docker run --net lnr-network -it -d --restart=always --name bestdist-client -h bestdist-client -p 3001:3001 -e HOST_NAME="prod" bestdist-client:0.0.0
+docker run --net lnr-network -it -d --restart=always --name bestdist-client -h bestdist-client -p 8080:8080 -e HOST_NAME="prod" bestdist-client:0.0.0
